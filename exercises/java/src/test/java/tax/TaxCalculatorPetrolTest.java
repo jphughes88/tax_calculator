fd7b114537cf63b0e7407350d94677a239f90d1c
Johnny <jphughes88@gmail.com>
2019-01-21 11:30:36 +0000

    Added TaxCalulatorPetrol

--- exercises/java/src/test/java/tax/TaxCalculatorPetrolTest.java
+++ exercises/java/src/test/java/tax/TaxCalculatorPetrolTest.java
@@ -10,7 +10,7 @@ import static java.time.Month.JANUARY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static tax.FuelType.PETROL;
 
-@Ignore
+
 public class TaxCalculatorPetrolTest {
 
     private static final LocalDate FIRST_OF_JAN_2019 = LocalDate.of(2019, JANUARY, 1);
@@ -18,7 +18,7 @@ public class TaxCalculatorPetrolTest {
 
     @Before
     public void setUp() {
-        taxCalculator = new DummyTaxCalculator();
+        taxCalculator = new TaxCalculatorPetrol();
     }
 
     @Test
